7936984226ed78ce121d5fa70f82abfdf5a5d4df
Saša Pozder <105084546+b3Vk7W2h7G8k0@users.noreply.github.com>
2022-07-21 18:42:10 +0100

    Add files via upload

---
+++ Iterable.py
@@ -0,0 +1,30 @@
+from typing import Iterable
+
+def repeated_mul(factor: int, modulus: int, times: int) -> Iterable[tuple[int,int]]:
+  state = 1
+  for i in range(1, times):
+    state = (state * factor) % modulus
+    yield (i, state)
+
+def dlog(p: int, g: int, h: int, B: int) -> int:
+  table = {}
+  for x1, y in repeated_mul(pow(g, -1, p), p, B):
+    table[h * y % p] = x1
+  for x0, y in repeated_mul(pow(g, B, p), p, B):
+    if y in table:
+      return (x0 * B + table[y]) % p
+  raise Exception("Could not find inverse?")
+
+def main() -> None:
+  p = 13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084171
+  g = 11717829880366207009516117596335367088558084999998952205599979459063929499736583746670572176471460312928594829675428279466566527115212748467589894601965568
+  h = 3239475104050450443565264378728065788649097520952449527834792452971981976143292558073856937958553180532878928001494706097394108577585732452307673444020333
+  B = 1 << 20
+
+  x = dlog(p, g, h, B)
+  assert pow(g, x, p) == h
+
+  print(x)
+
+if __name__ == "__main__":
+  main()
